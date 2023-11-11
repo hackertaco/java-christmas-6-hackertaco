c0609670be6cf6f918fde96c1fe0003625befa73
hackertaco <imsa1224@korea.ac.kr>
2023-11-12 04:21:12 +0900

    feat(Messages): add Messages

---
+++ src/main/java/christmas/utils/Messages.java
@@ -0,0 +1,21 @@
+package christmas.utils;
+
+public class Messages {
+    static public final String WELCOME = "안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다.\n";
+    static public final String REQUEST_RESERVATION_DATE = "%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n";
+    static public final String REQUEST_ORDER = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+
+    public static final String MENU_HEADER = "<주문 메뉴>";
+    public static final String TOTAL_ORDER_AMOUNT_HEADER = "<할인 전 총주문 금액>";
+    public static final String GIFT_MENU_HEADER = "<증정 메뉴>";
+    public static final String NO_GIFT_MESSAGE = "없음";
+    public static final String BENEFIT_LIST_HEADER = "<혜택 내역>";
+    public static final String NO_BENEFIT_MESSAGE = "없음";
+    public static final String TOTAL_BENEFIT_AMOUNT_HEADER = "<총혜택 금액>";
+    public static final String AFTER_DISCOUNT_AMOUNT_HEADER = "<할인 후 예상 결제 금액>";
+    public static final String MONTHLY_BADGE_HEADER = "<%d월 이벤트 배지>\n";
+
+    public static final String MONEY_UNIT = "%d원%n";
+
+    public static final String GIFT_DETAIL = "%s %d개\n";
+}
