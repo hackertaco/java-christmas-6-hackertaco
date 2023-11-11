968dd7b4ef8a5a636ef53c30842123b0456330e1
hackertaco <imsa1224@korea.ac.kr>
2023-11-12 04:26:04 +0900

    feat: add models: Order, OrderDetails, Reservation

---
+++ src/main/java/christmas/model/Order.java
@@ -0,0 +1,82 @@
+package christmas.model;
+
+import static christmas.config.RuleConfig.MENU_DIVIDER;
+import static christmas.config.RuleConfig.MIN_ORDER_COUNT;
+import static christmas.utils.ErrorMessages.INVALID_ORDER;
+
+import java.util.Arrays;
+
+
+public class Order {
+    private Menu menu;
+    private int count;
+
+    private Order(String menuName, String menuCount){
+        menu = Arrays.stream(Menu.values()).filter(m->m.getName().equals(menuName)).findAny().get();
+        count = Integer.parseInt(menuCount);
+    }
+
+    public static Order createOrder(String menuBeforeValidated){
+            haveMenuFormat(menuBeforeValidated);
+            canDivideInto(menuBeforeValidated);
+
+            String menuName = menuBeforeValidated.split(MENU_DIVIDER)[0];
+            String menuCount = menuBeforeValidated.split(MENU_DIVIDER)[1];
+
+            validateMenu(menuName);
+            validateMenuCount(menuCount);
+
+            return new Order(menuName, menuCount);
+    }
+
+    public Menu getMenu(){
+        return menu;
+    }
+    public int getCount(){
+        return count;
+    }
+
+    public int getPrice(){
+        return menu.getPrice() * count;
+    }
+
+    @Override
+    public String toString(){
+        return menu.getName()+ " " +count+"개";
+    }
+    private static void validateMenu(String menuNameBeforeValidated){
+        if(!isAvailableMenu(menuNameBeforeValidated)){
+            throw new IllegalArgumentException(INVALID_ORDER);
+        }
+    }
+    private static void haveMenuFormat(String menuBeforeValidated) {
+        if(!menuBeforeValidated.contains(MENU_DIVIDER)){
+            throw new IllegalArgumentException(INVALID_ORDER);
+        }
+    }
+
+    private static void canDivideInto(String menuBeforeValidated) {
+        if(menuBeforeValidated.split(MENU_DIVIDER).length != 2){
+            throw new IllegalArgumentException(INVALID_ORDER);
+        }
+    }
+    private static boolean isAvailableMenu(String menuNameBeforeValidated){
+        return Arrays.stream(Menu.values()).anyMatch(s -> s.getName().equals(menuNameBeforeValidated));
+    }
+
+    private static void validateMenuCount(String menuCountBeforeValidated){
+        checkZeroCount(parseStringToInt(menuCountBeforeValidated));
+    }
+    private static int parseStringToInt(String menuCountBeforeValidated){
+        try {
+            return Integer.parseInt(menuCountBeforeValidated);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException(INVALID_ORDER);
+        }
+    }
+    private static void checkZeroCount(int count){
+        if(count < MIN_ORDER_COUNT){
+            throw new IllegalArgumentException(INVALID_ORDER);
+        }
+    }
+}
---
+++ src/main/java/christmas/model/Reservation.java
@@ -0,0 +1,84 @@
+package christmas.model;
+
+import static christmas.config.RuleConfig.GIVING_EVENT_MIN_BOUND;
+import static christmas.config.RuleConfig.WEEKDAY_DISCOUNT_MENU_GROUP;
+import static christmas.config.RuleConfig.WEEKEND_DISCOUNT_MENU_GROUP;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Reservation {
+    OrderDetails orderDetails;
+    ReservationDate date;
+    List<EventDetail> events;
+
+    public Reservation(OrderDetails orderDetails, ReservationDate date){
+        this.date = date;
+        this.orderDetails = orderDetails;
+        this.events = new ArrayList<>();
+    }
+    public void createEvents(){
+        checkWeekendDiscountEvent();
+        checkWeekdayDiscountEvent();
+        checkDdayEvent();
+        checkSpecialEvent();
+        checkGivingEvent();
+    }
+    private void checkWeekendDiscountEvent(){
+        int count = countTargetMenu(MenuGroup.valueOf(WEEKEND_DISCOUNT_MENU_GROUP));
+        if(date.isWeekend() & count > 0){
+            events.add(EventDetail.createEvent(Event.WEEKEND, count));
+        }
+    }
+    private void checkWeekdayDiscountEvent(){
+        int count = countTargetMenu(MenuGroup.valueOf(WEEKDAY_DISCOUNT_MENU_GROUP));
+        if(!date.isWeekend() && count > 0){
+            events.add(EventDetail.createEvent(Event.WEEKDAY, count));
+        }
+    }
+    private void checkDdayEvent(){
+        if(date.isBeforeDday()){
+            events.add(EventDetail.createEvent(Event.D_DAY, date.getParsedDateForDdayCalculate()));
+        }
+    }
+    private void checkSpecialEvent(){
+        if(date.isInSpecialDay()){
+            events.add(EventDetail.createEvent(Event.SPECIAL, 1));
+        }
+    }
+
+    private void checkGivingEvent(){
+        if(isTargetOfGivingEvent()){
+            events.add(EventDetail.createEvent(Event.GIFT, 1));
+        }
+    }
+
+    private int countTargetMenu(MenuGroup menuGroup) {
+        return orderDetails.countTargetMenu(menuGroup.getMenuGroup());
+    }
+
+    public List<String> getOrderDetails(){
+        return orderDetails.getOrders();
+    }
+
+    public int getTotalPrice(){
+        return orderDetails.countTotal();
+    }
+
+    public int getTotalEventPrice(){
+        return events.stream().mapToInt(EventDetail::getDiscountPrice).sum();
+    }
+    public boolean isTargetOfGivingEvent(){
+        return orderDetails.countTotal() >= GIVING_EVENT_MIN_BOUND;
+    }
+
+    public boolean isEventNone(){
+        return events.size() == 0;
+    }
+
+
+    public List<String> getEvents(){
+        return events.stream().map(EventDetail::toString).collect(Collectors.toList());
+    }
+}
