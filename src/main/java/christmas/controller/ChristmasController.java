8ff128e6e8766fec55f03201fd65054ec2e714c2
hackertaco <imsa1224@korea.ac.kr>
2023-11-14 12:42:05 +0900

    refactor: make outputview get showResult responsibility

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -3,7 +3,6 @@ package christmas.controller;
 import static christmas.config.RuleConfig.TARGET_MONTH;
 import static christmas.utils.Messages.WELCOME;
 
-import christmas.model.Badge;
 import christmas.model.OrderDetails;
 import christmas.model.Reservation;
 import christmas.model.ReservationDate;
@@ -32,8 +31,7 @@ public class ChristmasController {
         Reservation reservation = new Reservation(orderDetails, reservationDate);
 
         christmasService.createEvents(reservation);
-
-        showResult(reservation);
+        outputView.showResult(reservation);
         inputView.readClose();
     }
 
@@ -43,19 +41,6 @@ public class ChristmasController {
 
     private OrderDetails getOrders(){
         return getInputWithRetry(inputView::readOrder, christmasService::createOrderDetails);
-    }
-
-    private void showResult(Reservation reservation){
-        int totalPrice = reservation.getTotalPrice();
-        int totalEventPrice = reservation.getTotalEventPrice();
-        String badge = Badge.getBadge(totalEventPrice);
-        outputView.showMenu(reservation.getOrderDetails());
-        outputView.showTotal(totalPrice);
-        outputView.showGift(reservation);
-        outputView.showTotalEventList(reservation);
-        outputView.showTotalEventPrice(totalEventPrice);
-        outputView.showPriceAfterDiscount(totalPrice - totalEventPrice);
-        outputView.showBadge(badge);
     }
 
     private <T, R> T getInputWithRetry(Supplier<R> inputReader, Function<R, T> creator) {
@@ -68,5 +53,4 @@ public class ChristmasController {
             }
         }
     }
-
 }
